e4a136603ae22d532585f9263c273038b9d26ed2
Maria Kezhun <marusa.kezhoon@yandex.ru>
2023-06-15 12:23:03 +0300

    15.06.23 2

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,11 +1,9 @@
 const Card = require("../models/card");
-const {
-  ERROR_INTERNAL_SERVER,
-  ERROR_BAD_REQUEST,
-  ERROR_NOT_FOUND,
-  STATUS_CREATED,
-  STATUS_OK,
-} = require("../utils/status");
+const ERROR_INTERNAL_SERVER = 500;
+const ERROR_BAD_REQUEST = 400;
+const ERROR_NOT_FOUND = 404;
+const STATUS_CREATED = 201;
+const STATUS_OK = 200;
 
 const getCard = (req, res) => {
   Card.find({})

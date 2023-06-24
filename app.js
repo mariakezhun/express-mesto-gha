bb85ad7b6b6a799acd5c9afdd825bd562578eda1
Maria Kezhun <marusa.kezhoon@yandex.ru>
2023-06-24 22:06:03 +0300

    24.06.23 5

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const routes = require("./routes/index");
-const { login, createUser } = require("./controllers/users");
 const auth = require("./middlewares/auth");
+const { errors } = require('celebrate');
 // const cookieParser = require('cookie-parser')
 
 const { PORT = 3000 } = process.env;
@@ -15,6 +15,7 @@ const app = express();
 // app.use(cookieParser());
 
 app.use(bodyParser.json());
+app.use(errors())
 
 // app.post("/signin", login);
 // app.post("/signup", createUser);
